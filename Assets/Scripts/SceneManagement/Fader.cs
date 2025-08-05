8d1ae8dfcdc892b375f74f005da0d08cd92c5898
keyyzo <58720632+keyyzo@users.noreply.github.com>
2025-08-05 21:42:15 +0100

    implementing the fader as a persistent object

--- Assets/Scripts/SceneManagement/Fader.cs
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -14,15 +14,34 @@ namespace RPG.SceneManagement
         {
             canvasGroup = GetComponent<CanvasGroup>();
 
-            StartCoroutine(FadeOut(fadeOutTime));
+            StartCoroutine(FadeOutIn());
         }
 
-        IEnumerator FadeOut(float timeForFade)
+        IEnumerator FadeOutIn()
         {
-            while (canvasGroup.alpha != 1) // alpha is not 1
+            yield return FadeOut(fadeOutTime);
+            Debug.Log("Faded out");
+            yield return FadeIn(fadeInTime);
+            Debug.Log("Faded in");
+        }
+
+        public IEnumerator FadeOut(float timeForFade)
+        {
+            while (canvasGroup.alpha < 1) // alpha is not 1
             { 
                 // moving alpha towards 1
-                canvasGroup.alpha = Time.deltaTime / timeForFade;
+                canvasGroup.alpha += Time.deltaTime / timeForFade;
+
+                yield return null;
+            }
+        }
+
+        public IEnumerator FadeIn(float timeForFade)
+        {
+            while (canvasGroup.alpha > 0) // alpha is not 0
+            {
+                // moving alpha towards 0
+                canvasGroup.alpha -= Time.deltaTime / timeForFade;
 
                 yield return null;
             }

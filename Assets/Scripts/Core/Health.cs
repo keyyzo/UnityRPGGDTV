c209c03b0950db4735abafd52d9b8aae38b9c94d
keyyzo <58720632+keyyzo@users.noreply.github.com>
2025-08-14 00:46:27 +0100

    updated the health component to save and load

--- Assets/Scripts/Core/Health.cs
+++ Assets/Scripts/Core/Health.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.Core
 {
-    public class Health : MonoBehaviour
+    public class Health : MonoBehaviour, ISaveable
     {
         [SerializeField] float healthPoints = 100.0f;
 
@@ -40,6 +41,21 @@ namespace RPG.Core
 
             animator.SetTrigger("die");
             actionScheduler.CancelCurrentAction();
+        }
+
+        public object CaptureState()
+        {
+            return healthPoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            healthPoints = (float)state;
+
+            if (healthPoints == 0)
+            {
+                ProcessDeath();
+            }
         }
     }
 }
